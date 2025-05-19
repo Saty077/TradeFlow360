df544bcd180489770c1e1ec09c93dcf15cad8261
Satyam Jha <satyamjha2743@gmil.com>
2025-05-19 11:28:53 +0530

    database connected with express

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+const uri = process.env.MONGO_URL;
+
+app.listen(PORT, () => {
   console.log("App Started");
+  mongoose
+    .connect(uri)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => {
+      console.log("MongoDB Connection Error: ", err.message);
+    });
 });
