4af0b2e1a6b66411cf4b9ca068ed5077655a04de
Satyam Jha <satyamjha2743@gmil.com>
2025-05-26 22:57:54 +0530

    functionality setup for BuyWindow, sending BuyStock to DB

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import GeneralContext from "./GeneralContext";
 
 import "./BuyActionWindow.css";
 
 const BuyActionWindow = ({ uid }) => {
+  const [stockQuantity, setStockQuantity] = useState(1);
+  const [stockPrice, setStockPrice] = useState(0.0);
+
+  const handleBuyClick = () => {
+    axios.post("http://localhost:3001/newOrder", {
+      name: uid,
+      qty: stockQuantity,
+      price: stockPrice,
+      mode: "Buy",
+    });
+    closeBuyWindow();
+  };
+
+  const { closeBuyWindow } = useContext(GeneralContext);
   const handleCancelClick = () => {
-    GeneralContext.closeBuyWindow();
+    closeBuyWindow();
   };
 
   return (
@@ -16,11 +32,24 @@ const BuyActionWindow = ({ uid }) => {
         <div className="inputs">
           <fieldset>
             <legend>Qty.</legend>
-            <input type="number" name="qty" id="qty" />
+            <input
+              type="number"
+              name="qty"
+              id="qty"
+              onChange={(e) => setStockPrice(e.target.value)}
+              value={stockPrice}
+            />
           </fieldset>
           <fieldset>
             <legend>Price</legend>
-            <input type="number" name="price" id="price" step="0.05" />
+            <input
+              type="number"
+              name="price"
+              id="price"
+              step="0.05"
+              onChange={(e) => setStockQuantity(e.target.value)}
+              value={stockQuantity}
+            />
           </fieldset>
         </div>
       </div>
@@ -28,7 +57,9 @@ const BuyActionWindow = ({ uid }) => {
       <div className="buttons">
         <span>Margin required 140.65</span>
         <div>
-          <Link className="btn btn-blue">Buy</Link>
+          <Link className="btn btn-blue" onClick={handleBuyClick}>
+            Buy
+          </Link>
           <Link to="" className="btn btn-grey" onClick={handleCancelClick}>
             Cancel
           </Link>
