b3bacc9bffd9b1f12f6aba9440e3bf5721f82edc
Satyam Jha <satyamjha2743@gmil.com>
2025-06-02 10:16:46 +0530

    implement user login in backend

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -1,7 +1,8 @@
-const { Signup } = require("../controllers/AuthController");
+const { Signup, Login } = require("../controllers/AuthController");
 const express = require("express");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
 
 module.exports = router;
