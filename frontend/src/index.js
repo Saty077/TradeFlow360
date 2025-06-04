97506e5de9b945f3e6824468cb1fba9565774e35
Satyam Jha <satyamjha2743@gmil.com>
2025-06-04 13:19:12 +0530

    Add login/signup functionality and integrate routes for authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import HomePage from "./Landing_page/Home/HomePage";
-import SignUp from "./Landing_page/signup/SignUp";
+
+import { CookiesProvider } from "react-cookie";
+import Login from "./Landing_page/auth/Login";
+import Signup from "./Landing_page/auth/Signup";
+
 import ProductPage from "./Landing_page/products/ProductPage";
 import PricingPage from "./Landing_page/pricing/PricingPage";
 import AboutPage from "./Landing_page/about/AboutPage";
@@ -15,17 +19,23 @@ import NotFound from "./Landing_page/NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/product" element={<ProductPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/pricing" element={<PricingPage />} />
-      <Route path="/support" element={<SupportPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
+  <CookiesProvider>
+    <BrowserRouter>
+      <Navbar />
+      <div className="content-wrapper">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/product" element={<ProductPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/pricing" element={<PricingPage />} />
+          <Route path="/support" element={<SupportPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  </CookiesProvider>
 );
