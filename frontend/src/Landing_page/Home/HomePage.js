b3d246158be6ceb3d8feb42e74c533bf4be50ef1
Satyam Jha <satyamjha2743@gmil.com>
2025-06-26 18:47:51 +0530

    update: add dashbord ping update in frontend

--- frontend/src/Landing_page/Home/HomePage.js
+++ frontend/src/Landing_page/Home/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "./Hero";
 import Awards from "./Awards";
 import Stats from "./Stats";
@@ -8,6 +8,18 @@ import Educations from "./Education";
 import OpenAccount from "../OpenAccount";
 
 function HomePage() {
+  const dashboardURL = process.env.REACT_APP_DASHBOARD_URL;
+  useEffect(() => {
+    if (dashboardURL) {
+      fetch(`${dashboardURL}/ping`, {
+        method: "GET",
+        mode: "no-cors",
+      }).catch((err) => {
+        console.log("Dashboard wake-up failed (non-blocking):", err);
+      });
+    }
+  }, [dashboardURL]);
+
   return (
     <>
       <Hero />
